a639af00ceb2a0a66a4475332a059ebedfb0455e
TOVTC <veronica.tc.to@outlook.com>
2022-03-13 04:46:36 -0400

    added randomizer and first draft of password loop - still to debug

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,90 @@
 // Assignment code here
+//password character arrays
+lowercaseLetters = ["a", "b", "c", "c", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+uppercaseLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "v", "W", "X", "Y", "Z"]
+numericalValues = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"] //maybe can remove to just use randomNumber
+specialCharacters = ["@", "%", "+", "/", "'", "!", "#", "$", "^", "?", ":", ",", "~", "-", "_", "."]
 
-//add code that links event to initiation of questions (welcome, answer questions to get your customized password - then hit password)
+
+//add code that links event to initiation of questions
 window.alert("Welcome to password generator! Answer the following questions to tailor your password.");
-debugger;
-var password = function(){
+var passwordGenerator = function(){
   //generate prompt to enter a value between 8 and 128
   var passwordLength = window.prompt("How many characters should your password have? Enter a value between 8 and 128.")
   //if the user entered an invalid length, self-invoke function
   if (passwordLength < 8 || passwordLength > 128) {
     window.alert("ERROR - You must enter a value between 8 and 128.")
-    password()
+    passwordGenerator()
   }
   //confirm whether user wants different types of characters
   var lowercaseConfirm = window.confirm("Should your password include lowercase letters?")
   var uppercaseConfirm = window.confirm("Should your password include uppercase letters?")
   var numericalConfirm = window.confirm("Should your password include numerical digits?")
   var specialConfirm = window.confirm("Should your password include special characters?")
-  //if the user answered no to all character types, a password cannot be generated, so self-invoke the function
+  //if the user answered no to all character types, a password cannot be generated, so self-invoke function
   if (!lowercaseConfirm && !uppercaseConfirm && !numericalConfirm && !specialConfirm) {
     window.alert("ERROR - You must select at least one character type");
-    password();
+    passwordGenerator();
   }
-  console.log(passwordLength, lowercaseConfirm, uppercaseConfirm, numericalConfirm, specialConfirm);
-}
+  debugger;
+  //create an array to hold the password
+  var password = []
+  //loop through generating a value for each character in the password
+  var characterLoop = function() {
+    characterType = randomNumber(1, 4);
+    if (characterType = 1 && lowercaseConfirm) {
+      index = randomNumber(1, 26);
+      value = lowercaseLetters[index];
+      password[i] = value;
+    } else {
+      characterLoop();
+    }
+    if (characterType = 2 && uppercaseConfirm) {
+      index = randomNumber(1, 26);
+      value = uppercaseLetters[index];
+      password[i] = value;
+    } else {
+      characterLoop();
+    }
+    if (characterType = 3 && numericalConfirm) {
+      index = randomNumber(1, 10);
+      value = uppercaseLetters[index];
+      password[i] = value;
+    } else {
+      characterLoop();
+    }
+    if (characterType = 4 && specialConfirm) {
+      index = randomNumber(1, specialCharacters.length);
+      value = uppercaseLetters[index];
+      password[i] = value;
+    } else {
+      characterLoop();
+    }
+  }
+      //loop me!
+    for (var i = 0; i < passwordLength; i++) {
+      characterLoop();
+    }
+  }
+  console.log(password)
 
+//create some sort of container for my values - an array?
+//create an array that holds all possible values for each category of character
 //generate a randomizer to determine whether the digit will be lowercase, uppercase, numeric, or special characters
 //generate for loop for the length of the selected password to pick a random value
-//use Math.random() and then use if value is ><= to separate into four categories, each representing one character type
+//use randomNumber = function (min, max) and then use if value is ><= to separate into four categories, each representing one character type
 //then use and && operator conditional to skip through the options
-//record each of the values in an array(?) that can be printed
+//use randomNumber and include min max values per character type to select the character to be added from the existing arrays
 
-//bonus - if they hit cancel for the number of characters, exit the loop
+  //loop through generating a value for each character in the password
+//    for (var i = 0; i < passwordLength; i++) {
+//      number = randomNumber(1, 4);
+//      password[i] = number;
+//    }
+//    console.log(password)
+//}
+
+//bonus - if they hit cancel for the number of characters, confirm if they want to exit the loop
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -47,5 +101,11 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+//generate a random value between two numbers
+var randomNumber = function(min, max){
+  var number = Math.floor(Math.random() * (max - min + 1) + min);
+  return number;
+}
+
 //start generating!
-password();
+passwordGenerator();
