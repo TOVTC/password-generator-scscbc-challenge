930d3a5e6e52a7d29b344776a74ae230d008d15b
TOVTC <veronica.tc.to@outlook.com>
2022-03-13 15:27:56 -0400

    added return; to invalid responses to avoid double-looping when incorrect password length is provided

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,23 @@
 // Assignment code here
 //password character arrays
-lowercaseLetters = ["a", "b", "c", "c", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-uppercaseLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "v", "W", "X", "Y", "Z"]
-numericalValues = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
-specialCharacters = ["@", "%", "+", "/", "'", "!", "#", "$", "^", "?", ":", ",", "~", "-", "_", "."]
+lowercaseLetters = ["a", "b", "c", "c", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+uppercaseLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "v", "W", "X", "Y", "Z"];
+numericalValues = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
+specialCharacters = ["@", "%", "+", "/", "'", "!", "#", "$", "^", "?", ":", ",", "~", "-", "_", "."];
 
 //create an array to hold the password
-var passwordArray = []
+var passwordArray = [];
 
 debugger;
 //function to generate password
 var passwordGenerator = function(){
   //generate prompt to enter a value between 8 and 128
   var passwordLength = window.prompt("How many characters should your password have? Enter a value between 8 and 128.");
-    //if the user entered an invalid length, self-invoke function
-    if (passwordLength < 8 || passwordLength > 128) {
-      window.alert("ERROR - You must enter a value between 8 and 128.");
-      passwordGenerator()
-    }
+  //if the user entered an invalid length, self-invoke function
+  if (passwordLength < 2 || passwordLength > 128) {
+    window.alert("ERROR - You must enter a value between 8 and 128.");
+    return;
+  }
   //confirm whether user wants different types of characters
   var lowercaseConfirm = window.confirm("Should your password include lowercase letters?");
   var uppercaseConfirm = window.confirm("Should your password include uppercase letters?");
@@ -26,7 +26,7 @@ var passwordGenerator = function(){
   //if the user answered no to all character types, a password cannot be generated, so self-invoke function
   if (!lowercaseConfirm && !uppercaseConfirm && !numericalConfirm && !specialConfirm) {
     window.alert("ERROR - You must select at least one character type");
-    passwordGenerator();
+    return;
   }
   //loop through generating a value for each character in the password
   var characterLoop = function() {
@@ -51,14 +51,13 @@ var passwordGenerator = function(){
       passwordArray[i] = value;
     } else {
       characterLoop();
-    };
-  }
-      //loop me!
-    for (var i = 0; i < passwordLength; i++) {
-      characterLoop();
-      console.log(passwordArray);
     }
+  };
+  //where variable i is index 0 in the array, for the cases at which the index number is less than the password length, execute the loop, and increase the value of i by 1 each time
+  for (var i = 0; i < passwordLength; i++) {
+    characterLoop();
   }
+};
   // Get references to the #generate element ORIGINAL
   var generateBtn = document.querySelector("#generate");
 
