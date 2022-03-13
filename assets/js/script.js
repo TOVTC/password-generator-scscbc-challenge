7e5702e95f89edda08d3b7cc85ad059e06f5e917
TOVTC <veronica.tc.to@outlook.com>
2022-03-13 16:36:41 -0400

    minor edits to code

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,12 +12,13 @@ var passwordArray = [];
 var passwordGenerator = function(){
   //generate prompt to enter a value between 8 and 128
   var passwordLength = window.prompt("How many characters should your password have? Enter a value between 8 and 128.");
+  //if the user entered a value that is not an integer, generate error, self-invoke, then exit function
   if (isNaN(passwordLength)) {
     window.alert("ERROR - You must enter a numerical value");
     passwordGenerator();
     return;
   }
-  //if the user entered an invalid length, exit function
+  //if the user entered an invalid length, generate error, self-invoke, then exit function
   if (passwordLength < 8 || passwordLength > 128) {
     window.alert("ERROR - You must enter a value between 8 and 128.");
     passwordGenerator();
@@ -28,7 +29,7 @@ var passwordGenerator = function(){
   var uppercaseConfirm = window.confirm("Should your password include uppercase letters?");
   var numericalConfirm = window.confirm("Should your password include numerical digits?");
   var specialConfirm = window.confirm("Should your password include special characters?");
-  //if the user answered no to all character types, a password cannot be generated, exit function
+  //if the user answered no to all character types, a password cannot be generated, generate error, self-invoke, then exit function
   if (!lowercaseConfirm && !uppercaseConfirm && !numericalConfirm && !specialConfirm) {
     window.alert("ERROR - You must select at least one character type");
     passwordGenerator();
