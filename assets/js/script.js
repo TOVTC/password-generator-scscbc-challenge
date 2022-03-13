2f71dc12a28117cab2abd358da96bf4b76684fd5
TOVTC <veronica.tc.to@outlook.com>
2022-03-13 17:49:32 -0400

    '10' is not a digit

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@
 //password character arrays
 lowercaseLetters = ["a", "b", "c", "c", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 uppercaseLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "v", "W", "X", "Y", "Z"];
-numericalValues = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
+numericalValues = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 specialCharacters = ["@", "%", "+", "/", "'", "!", "#", "$", "^", "?", ":", ",", "~", "-", "_", "."];
 
 //create an array to hold the password
